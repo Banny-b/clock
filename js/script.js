95c5eca31f32a3993db2c25478eb17dbadec4a29
Sergey Petukhov <banny75@i.ua>
2023-05-30 23:53:04 +0300

    Digital clock
    add date

--- js/script.js
+++ js/script.js
@@ -4,18 +4,28 @@ function showTime() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     let ampm = "AM";
+
     // Определение AM или PM
     if (hours > 12) {
         hours -= 12;
         ampm = "PM";
     }
+
     // Добавление 0 перед однозначными цифрами
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
+
     // Отображение время в виде HH:MM:SS AM/PM
     const time = hours + ":" + minutes + ":" + seconds + " " + ampm;
     document.getElementById("clock").innerText = time;
+
+    // Получение текущей даты
+    const currentDate = date.toDateString();
+    document.getElementById("date").innerText = currentDate;
+
     // Обновление время каждую секунду
     setTimeout(showTime, 1000);
-};
+}
+
+showTime();
